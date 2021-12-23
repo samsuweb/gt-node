2e253ca9520ed2c39051b87d4ca9e0ee8e266674
samsuweb <86432077+samsuweb@users.noreply.github.com>
2021-12-23 12:10:57 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,3 @@
-var apm = require('elastic-apm-node').start({
-  serviceName: 'gt-node',
-  
-  // Set custom APM Server URL (default: http://localhost:8200)
-  serverUrl: 'http://<vm_ip>:30337',
-})
-var sleep = require('sleep');
-
 // Require the framework and instantiate it
 const fastify = require('fastify')({ logger: true })
 
@@ -13,17 +5,6 @@ const fastify = require('fastify')({ logger: true })
 fastify.get('/', async (request, reply) => {
   return { hello: 'world' }
 })
-
-fastify.get('/host', async (request, reply) => {
-  let os = require("os");
-  return { hostname: os.hostname() }
-})
-
-fastify.get('/sleep', async (request, reply) => {
-  sleep.sleep(10)
-  return { sleep_done: true }
-})
-
 
 // Run the server!
 const start = async () => {
